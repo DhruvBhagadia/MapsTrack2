239510215ebf006cbf9a6d3f7fa79b6a081cc7a6
DhruvBhagadia <dhruvbhagadia@gmail.com>
2018-01-28 12:41:03 +0530

    Added danger toast for a particular location

--- app/src/main/java/swappers/com/mapstrack/MapsActivity.java
+++ app/src/main/java/swappers/com/mapstrack/MapsActivity.java
@@ -21,6 +21,7 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
@@ -28,8 +29,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private GoogleMap mMap;
     LocationManager locationManager;
     LocationListener locationListener;
+    Double d1, d2, d3;
+    LatLng ll1 , ll2, ll3;
+    CalculateDistance calculateDistance = new CalculateDistance();
 
-    @Override
+
+  @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
@@ -72,19 +77,51 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         mMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
-
         locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
 
-        locationListener = new LocationListener() {
+
+        Marker m1 = mMap.addMarker(new MarkerOptions()
+            .position(new LatLng(37.787844, -122.391658))
+            .title("Tool Booth1"));
+
+        Marker m2 = mMap.addMarker(new MarkerOptions()
+            .position(new LatLng(37.783740, -122.394224))
+            .title("Title2"));
+
+        Marker m3 = mMap.addMarker(new MarkerOptions()
+            .position(new LatLng(37.788118, -122.388762))
+            .title("Title3"));
+
+      locationListener = new LocationListener() {
             @Override
             public void onLocationChanged(Location location) {
 
                 LatLng userLocation = new LatLng(location.getLatitude(), location.getLongitude());
+              Log.i("LOCATION",location.toString());
 
                 mMap.clear();
-                mMap.addMarker(new MarkerOptions().position(userLocation).title("YourLocation"));
+                mMap.addMarker(new MarkerOptions().position(userLocation).title("YourLocation")
+                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE)));
+                ll1 = new LatLng(37.787844, -122.391658);
+                ll2 = new LatLng(37.783740, -122.394224);
+                ll3 = new LatLng(37.788118, -122.388762);
+              mMap.addMarker(new MarkerOptions()
+                  .position(ll1)
+                  .title("Tool Booth1"));
+              mMap.addMarker(new MarkerOptions()
+                  .position(ll2)
+                  .title("Tool Booth2"));
+              mMap.addMarker(new MarkerOptions()
+                  .position(ll3)
+                  .title("Tool Booth3"));
                 //mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation,0));
                 Log.i("Location",location.toString());
+                d1 = calculateDistance.CalculationByDistance(userLocation, ll1);
+                Log.i("d1",d1.toString());
+              d2 = calculateDistance.CalculationByDistance(userLocation, ll1);
+              d3 = calculateDistance.CalculationByDistance(userLocation, ll1);
+              if (d1 <= 500 || d2 <= 500 || d3 <= 500)
+                Toast.makeText(MapsActivity.this, "Danger", Toast.LENGTH_SHORT).show();
             }
 
             @Override
@@ -101,6 +138,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             public void onProviderDisabled(String s) {
 
             }
+
         };
 
         if(Build.VERSION.SDK_INT < 23) {
@@ -121,8 +159,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                   Location lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
                   LatLng userLocation = new LatLng(lastKnownLocation.getLatitude(),lastKnownLocation.getLongitude());
 
+
                   mMap.clear();
-                  mMap.addMarker(new MarkerOptions().position(userLocation).title("YourLocation"));
+                  mMap.addMarker(new MarkerOptions().position(userLocation).title("YourLocation").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE)));
                   mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation,15));
 
             }
